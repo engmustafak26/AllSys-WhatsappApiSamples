749decb87d585d1124b09d65693b6fef1ecc9d5b
mustafa kamal <mkamal@sejeltech.com>
2024-06-23 03:00:45 +0300

    add caller code for GetOTPStatusApi, to get status of the sent message

--- WhatsappApi/Dto/Responses/EmptyResponseDto.cs
+++ WhatsappApi/Dto/Responses/EmptyResponseDto.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace WhatsappApi.Dto.Responses
 {
     public class EmptyResponseDto
     {
+    }
+
+    public class MessageIdDto
+    {
+        public long MessageId { get; set; }    
+    }
+
+    public class GetOtpMessageStatusResponseDto
+    {
+        public DateTime? SendDate { get; set; }
+        public int Status { get; set; }
+        public string StatusDescription { get; set; }
     }
 }
